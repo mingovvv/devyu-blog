e06294ef0c02e57f9a4568ec6f92920d5eaa125f
puregyu <puregyu@naver.com>
2020-01-16 12:17:32 +0900

    fragments update

--- devyu-blog/src/main/java/com/devyu/blog/DevyuBlogApplication.java
+++ devyu-blog/src/main/java/com/devyu/blog/DevyuBlogApplication.java
@@ -3,7 +3,6 @@ package com.devyu.blog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class DevyuBlogApplication {
 	public static void main(String[] args) {
